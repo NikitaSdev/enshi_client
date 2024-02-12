75afd5ee775a222457a4406054e1e43c42401dc8
Mr.KillMyself <nikitashadowrazev@gmail.com>
2024-02-12 02:58:44 +0100

    все готово

--- src/entites/home-slider/view.tsx
+++ src/entites/home-slider/view.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { IAnimeSlider } from "@/shared/types/slider.types";
 import Image from "next/image";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { BigSlide } from "./lib/big-slide";
 import { SmallSliderContainer, StyledContainer } from "./styles";
 import { Slider } from "@/shared/components/slider";
@@ -9,6 +9,15 @@ import { useMediaQuery } from "@mui/material";
 
 export const HomeSlider: FC<{ sliders: IAnimeSlider[] }> = ({ sliders }) => {
   const [selectedSlide, setSelectedSlide] = useState<IAnimeSlider>(sliders[0]);
+  const [isDataProceed, setIsDataProceed] = useState(false);
+  useEffect(() => {
+    if (!isDataProceed) {
+      if (sliders && sliders.length > 0) {
+        setIsDataProceed(true);
+        setSelectedSlide(sliders[0]);
+      }
+    }
+  }, [sliders]);
   const isMobile = useMediaQuery("(max-width: 420px)");
   const smallSlides = sliders.map((slide) => (
     <Image
